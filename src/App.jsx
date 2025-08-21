71226f3307b1c3397610d72ca4b7b99433b59a17
joaopug <41847866+joaopug@users.noreply.github.com>
2025-08-20 21:21:09 -0300

    Primeira atividade feita

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProdutoProps from "./Produto.jsx";
+
+function App() {
+  return (
+    <ProdutoProps
+      produto="produto"
+      nomeP="Banana"
+      precoP="R$ 100,00"
+    />
+  );
+}
+
+export default App
