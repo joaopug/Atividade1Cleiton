bf6d3cf8a7d05dcb6fbf8d7ddf19e70c9011c85a
joaopug <41847866+joaopug@users.noreply.github.com>
2025-08-20 22:03:19 -0300

    Exercício 1 e 2 concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
+import "./App.css";
+import PessoaProps from "./CartaoPessoa.jsx";
 import ProdutoProps from "./Produto.jsx";
 
 function App() {
   return (
-    <ProdutoProps
-      produto="produto"
-      nomeP="Banana"
-      precoP="R$ 100,00"
-    />
+    <>
+      <div className="App">
+        <ProdutoProps produto="produto" nomeP="Banana" precoP="R$ 100,00" />
+
+        <PessoaProps
+          classeNome="PESSOA-nome"
+          classeIdade="PESSOA-idade"
+          classeProfissao="PESSOA-profissao"
+          nomePes="Cleiton"
+          idadePes="30"
+          profissaoPes="Programador"
+        />
+      </div>
+    </>
   );
 }
 
-export default App
+export default App;
